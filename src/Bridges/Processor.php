fb46afa1bdf7783fd0f8d37529ae68f24359290b
Radek Fryšták <geniv.radek@gmail.com>
2018-02-07 12:36:34 +0100

    upgrade interface, update to more confirutable

---
+++ src/Bridges/Processor.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: geniv
+ * Date: 7.2.18
+ * Time: 12:26
+ */
+
+
+class Processor
+{
+
+}
---
+++ src/IProcessor.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: geniv
+ * Date: 7.2.18
+ * Time: 12:22
+ */
+
+namespace Calendar;
+
+
+class IProcessor
+{
+
+}
